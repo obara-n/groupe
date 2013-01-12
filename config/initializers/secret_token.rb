24e50f0f12e884f7b3f8a25f1f4162d5edc27c27
obara <a1212no@aiit.ac.jp>
2013-01-13 03:02:09 +0900

    generated myapp1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp1::Application.config.secret_token = 'a0bdf2d1db9cf0d49ab5e47f97a18d9aaaf693c7c3b723cf7b36d8d8f9bcdf3bfba7975a731a73b022c37c6a8d4dc6ce02f7e43927bd8faa65c324f4577491b4'
